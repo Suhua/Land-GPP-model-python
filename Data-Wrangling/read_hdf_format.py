d449406fba3d5aec41a2c3535d8749a9bca449e7
Suhua Wei <suhua.wei00@gmail.com>
2014-09-28 19:56:17 -0400

    Update read_hdf_format.py

--- Data-Wrangling/read_hdf_format.py
+++ Data-Wrangling/read_hdf_format.py
@@ -14,16 +14,16 @@ print num_files
 def read_lat_lon(lat,lon,lat_r,lon_r):
 '''read the lat and lon at specific lat and lon resolution(lat_r and lon_r), return the index of lat and lon at
 gridded data set, by default, lat ranges from -90 to 90 northward and lon ranges -180 to 180 eastward '''
-if (lat>=-90 and lat<=90):
-lat=90-lat
-if (lon>=-180 and lon<=180):
-lon=lon+180
-lat_ind=int(lat//lat_r)
-lon_ind=int(lon//lon_r)
-return lat_ind, lon_ind
-site=pd.read_csv('G:/Flux Data/flux_PP/siteInfo.csv')
-num_site=len(site['sitename'])
-print num_site
+    if (lat>=-90 and lat<=90):
+    lat=90-lat
+    if (lon>=-180 and lon<=180):
+    lon=lon+180
+    lat_ind=int(lat//lat_r)
+    lon_ind=int(lon//lon_r)
+    return lat_ind, lon_ind
+    site=pd.read_csv('G:/Flux Data/flux_PP/siteInfo.csv')
+    num_site=len(site['sitename'])
+    print num_site
 fpar_output=np.zeros((num_files,num_site),float)
 t_cnt=0
 for fn in files:
@@ -31,20 +31,20 @@ if fn.endswith('HDF'):
 data=SD.SD(fn).select('fapar').get()
 # data=ma.masked_where(data==255,data)
 for cnt in range(0,num_site,1):
-ilat=site['Lat'][cnt]
-ilon=site['Lon'][cnt]
-#print ilat,ilon
-# site_name=site['sitename'][cnt]
-#print site_name
-# pft=site['IGBP'][cnt]
-#cnt+=1
-# print pft
-lat_ind,lon_ind=read_lat_lon(ilat,ilon,0.5,0.5)
-#print lat_ind,lon_ind
-#data=spei_data[:,lat_ind,lon_ind]
-fpar_output[t_cnt,cnt]=data[lat_ind,lon_ind]
-t_cnt+=1
-print fpar_output[-10:]
+    ilat=site['Lat'][cnt]
+    ilon=site['Lon'][cnt]
+    #print ilat,ilon
+    # site_name=site['sitename'][cnt]
+    #print site_name
+    # pft=site['IGBP'][cnt]
+    #cnt+=1
+    # print pft
+    lat_ind,lon_ind=read_lat_lon(ilat,ilon,0.5,0.5)
+    #print lat_ind,lon_ind
+    #data=spei_data[:,lat_ind,lon_ind]
+    fpar_output[t_cnt,cnt]=data[lat_ind,lon_ind]
+    t_cnt+=1
+    print fpar_output[-10:]
 np.save('fPAR.npy',fpar_output)
 fpar_output=ma.masked_where(fpar_output==255,fpar_output)
 # plot the image
