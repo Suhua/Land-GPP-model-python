92ad29a9a2b20c9f1c1524cf27f52a0f720d61ee
Suhua Wei <suhua.wei00@gmail.com>
2014-09-30 20:58:35 -0400

    Update curve_fitting.py

--- Model-Develope/curve_fitting.py
+++ Model-Develope/curve_fitting.py
@@ -1,10 +1,24 @@
+import time
+import numpy as np
 
-def func(x,a,b,c):
-    return c-x*b/(x+a)
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+ticks=time.clock()
+
+nee=np.load('nee_US_KS2.npy')
+ppfd=np.load('ppdf_US_KS2.npy')
+
+
+# ppfd=np.ma.masked_where(ppfd,ppfd<0)
+# nee=np.ma.masked_where(nee,ppfd<0)
+print nee, ppfd
+def func(x,a,b):
+    return b-a*x
 popt,pcov=curve_fit(func,ppfd,nee)
 print popt
-# except:
-#     print "Unexpected error:", sys.exc_info()[0]
-# else:
-#     print "done with no exceptions"
+
+print np.sqrt(np.diag(pcov))
+
+plt.plot(ppfd,nee,'b*',ppfd,popt[1]-popt[0]*ppfd,'r--')
+plt.show()
 print time.clock()-ticks
