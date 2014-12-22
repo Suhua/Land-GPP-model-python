76620199990cf25b9e6e33bfd178364034fad6c1
Suhua Wei <suhua.wei00@gmail.com>
2014-12-22 12:59:37 -0500

    Update PP_PAR_DF.py

--- Photosynthetic-parameters/PP_PAR_DF.py
+++ Photosynthetic-parameters/PP_PAR_DF.py
@@ -24,14 +24,15 @@ gridded data set, by default, lat ranges from -90 to 90 northward and lon ranges
     return lat_ind, lon_ind
 
 
-## Set working directory
+'''Set data reading directory'''
 work_dir=r'G:\Research\Gridded Data\NC\NC_data'
 os.getcwd()
 os.chdir(work_dir)
 f=netcdf.netcdf_file('nswrs.sfc.mon.mean.nc','r')
 # print f.dimensions,'\n', f.history,'\n'
 
-# Read the variables
+'''Read the variables, net short wave radiation (nswrs) data set has three dimensions: time, latitdues and longitudes, 
+spatial resolution is not 0.5 x 0.5 degree, so we need to resize it to match the targeted resolution'''
 time=f.variables['time']
 lats=f.variables['lat']
 lons=f.variables['lon']
@@ -39,17 +40,17 @@ Var=f.variables['nswrs']
 # print dir(Var),'\n', Var.add_offset,Var.scale_factor,'\n',Var.shape
 
 
-# Par is considered as 45% of the net shortwave radiation
+'''Par is estimated as 45% of the net shortwave radiation'''
 data=Var[672:696,:,:]*0.45
 
 
-## extract tower point data from resized data
+''' read the fluxnet tower locations (latitudes and longitudes)'''
 site=pd.read_csv(r'G:\Research\fluxdata\Flux Data\flux_PP\KoppenClimateAnalysis\PP_JJA.csv')
 siteID=site['siteID']
 num_site=len(siteID)
-Par_output=np.zeros((24,num_site),float) # initialize the output matrix
+Par_output=np.zeros((24,num_site),float)  
 
-#  Resize the data into 0.5x0.5 degree ([360,720])
+''' Resize the data into 0.5x0.5 degree ([360,720])'''
 for i in range(0,data.shape[0]):
     sr=sm.imresize(data[i,:,:],[360,720],interp='nearest')
     for cnt in range(0,num_site): # loop through all the sites
@@ -59,7 +60,7 @@ for i in range(0,data.shape[0]):
         Par_output[i,cnt]=sr[ilat,ilon]
 
 
-# save the PAR data into DataFrame
+'''save the PAR data into DataFrame'''
 month=list(range(0,24))
 month[0]=datetime.date(2004,1,1).isoformat()
 i=0
