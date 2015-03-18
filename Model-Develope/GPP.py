5cb0401e3b2cd73ea814319fb9fff0f00f01b0e8
Sue Wei <suhua.wei00@gmail.com>
2015-03-18 17:54:40 -0400

    Update GPP.py

--- Model-Develope/GPP.py
+++ Model-Develope/GPP.py
@@ -5,6 +5,7 @@ from scipy.ndimage import zoom
 import matplotlib.pylab as plt
 import glob
 from pyhdf import SD,HDF
+from mpl_toolkits import basemap
 
 class GPP:
     '''GPP (Gross Primary Production) is calculated as GPP=alpha*fPAR*PAR; alpha is the specific light use efficiency at monthly step;PAR is partial
@@ -55,12 +56,28 @@ for month in range(1,13):
     alpha=GPP.Alpha(month)
     # print alpha
     output=alpha*fpar*PAR
+    fig = plt.figure()
+    ax = fig.add_axes([0.05,0.05,0.9,0.9])
+    map=np.ma.fix_invalid(output,copy=True,fill_value=0)
+  
+    lats=np.linspace(90,-90,360)
+    lons=np.linspace(-180,180,720)
+    lons, lats = np.meshgrid(lons,lats)
+    m =basemap.Basemap(projection='robin',lon_0=0.0,resolution=None)
+    im1= m.pcolormesh(lons,lats,map,shading='flat',cmap=plt.cm.jet,latlon=True)
+    m.drawparallels(np.arange(-90.,99.,30.))
+    m.drawmeridians(np.arange(-180.,180.,60.))
+    mstr=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+    ax.set_title(''.join(['Global Gross Primary Production in ' , mstr[month-1]]))
+    cb = m.colorbar(im1,"bottom", size="5%", pad="2%")
+    plt.savefig(''.join(['Global Gross Primary Production in ' , mstr[month-1],'.jpg']))
+    # plt.show()
+    
     area_weight=io.loadmat(r'G:\Research\Gridded Data\NC\area_360x720.mat')
     area=area_weight['area_global']
     output=output*area/1000000000000000
     total_GPP=total_GPP+np.sum(np.sum(output))*30
-    # plt.imshow(output)
-    # plt.colorbar()
-    # plt.show()
+
+
 
 print total_GPP*0.85
