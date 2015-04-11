c69f6825d907089830fc907290423a208ef548e4
Sue Wei <suhua.wei00@gmail.com>
2015-04-11 13:53:02 -0400

    Update Download_URL.py
    A script to download MODIS EVI(enhanced vegetation index) data.

--- Data-Wrangling/Download_URL.py
+++ Data-Wrangling/Download_URL.py
@@ -20,18 +20,14 @@ def file_date_name(year,month,day):
 URL = 'http://e4ftl01.cr.usgs.gov/MOLT/MOD15A2.005/'
 
 st=datetime.date(2003,1,01)  
-for i in range(100): # Input the number of files to retrieve
+for i in range(240): # Input the number of files to retrieve
     path_name= os.path.join(URL,file_date_name(st.year,st.month,st.day))
-    print "the {0} th file".format(i)
     if (st.month==12 and st.day>23): # Change into next year
         st=datetime.date(st.year+1,1,01)
     else:
         # path_name= os.path.join(URL,file_date_name(st.year,st.month,st.day))
         st=st+datetime.timedelta(days=8) # Add 8-days increase to st
     response=urllib.urlopen(path_name)
-    
-    
-    
     for line in response:
         temElement=line.split('\"')
         for name in temElement:
@@ -39,5 +35,5 @@ for i in range(100): # Input the number of files to retrieve
             # if name[:20]>='MOD15A2.A2003001.h26' and name.endswith('hdf'):
                 file_path=os.path.join(path_name,name)
                 urllib.urlretrieve(file_path,name)
-                print file_path
+                print 'done with downloading  "+file_path
 
