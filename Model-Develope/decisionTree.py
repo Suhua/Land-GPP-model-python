55e9cfc4bf4c8f355765bfe69f9528456eee1b28
Sue Wei <suhua.wei00@gmail.com>
2015-05-07 18:09:00 -0400

    Update decisionTree.py

--- Model-Develope/decisionTree.py
+++ Model-Develope/decisionTree.py
@@ -1,3 +1,4 @@
+__author__ = 'swei'
 import numpy as np
 import pandas as pd
 import scipy.misc as sm
@@ -28,6 +29,20 @@ class GridData():
         ta[: , 360:] = temp[: , :360]
         ta[: , :360] = temp[: , 360:]
         return ta[: , :]
+
+    def fPAR(self,month,year):
+        '''fPar is an unitless value ranging from 0 to 1, with monthly step and 0.5 x 0.5 spatial resolution'''
+        month=month
+        hdf_files = glob.glob(r'G:\Research\Gridded Data\Vegetation Index\LAI and FPAR\fPAR200{0}\*HDF'.format(year))
+        cnt = 0
+        for fn in hdf_files:
+            if fn.endswith('HDF'):
+                cnt += 1
+                if cnt == int(month) + 1:
+                    # print cnt
+                    data = SD.SD(fn).select('fapar').get()
+                    data = np.ma.masked_where(data == 255, data) / 255.0
+                    return data
 
     def evi(self):
         '''fPar is an unitless value ranging from 0 to 1, with monthly step and 0.5 x 0.5 spatial resolution'''
@@ -79,18 +94,25 @@ class TowerData():
     #     self.siteName=siteName
     #     self.lat=lat
     #     self.lon=lon
-
     def getGpp(self,year):
         year=str(year)
         df=pd.read_csv("G:\Research\modelling\machine learning\gpp.monthly.csv")
         # print df.columns.tolist
         gppYear={}
         totalSite=0
+        for item in df.columns:
+            if np.isnan(df.ix[0, item]):
+                df.ix[0, item] = 0
+
+        df = df.interpolate(method='linear', axis=1)
         for item in df.columns[1:]:
             # print item
             site,yr=item.split('.')
             if yr==year:
-               if np.mean(df[item].as_matrix())>0:
+               dfSite=df[item].interpolate(method='linear')
+               if np.any(dfSite.as_matrix() is np.nan):
+                   pass
+               else:
                    totalSite+=1
                    gppYear[site]=df[item].as_matrix()
         return gppYear,totalSite
@@ -111,106 +133,114 @@ def gridToPoint(lats, lons):
             lon_ind.append(ind)
     return lat_ind, lon_ind
 
-for year in range(2000,2007):
+for year in range(2000,2006):
     total_GPP=0
-    for iter in range(0, 20):
-        for month in range(1,13):
-            T = TowerData()
-            gpp,totalSite= T.getGpp(year)
-            features=np.zeros([totalSite,6],dtype=float)
-            sitNam=gpp.keys()
+
+    for month in range(1,13):
+        T = TowerData()
+        gpp,totalSite= T.getGpp(year)
+        features=np.zeros([totalSite,7],dtype=float)
+        sitNam=gpp.keys()
 
 
-            target=[]
-            for site in sitNam:
-                target.append(gpp[site][month-1])
+        target=[]
+        for site in sitNam:
+            target.append(gpp[site][month-1])
 
-            df_info=pd.ExcelFile(r"G:\Research\modelling\machine learning\fluxnet_2004-2005-monthly.xlsx",header=0,index_col=0,parse_cols=0,has_index_name=True)
-            df_info=pd.ExcelFile.parse(df_info,index_col=0,header=0)
-            df = df_info[df_info.mm==1]
-            lats=[]
-            lons=[]
-            sites=[]
-            for site in sitNam:
-                if site in df.index:
-                   lats.append(df[df.index==site]['latitude'][0])
-                   lons.append(df[df.index==site]['longitude'][0])
-                   sites.append(site)
+        df_info=pd.ExcelFile(r"G:\Research\modelling\machine learning\fluxnet_2004-2005-monthly.xlsx",header=0,index_col=0,parse_cols=0,has_index_name=True)
+        df_info=pd.ExcelFile.parse(df_info,index_col=0,header=0)
+        df = df_info[df_info.mm==1]
+        lats=[]
+        lons=[]
+        sites=[]
+        for site in sitNam:
+            if site in df.index:
+               lats.append(df[df.index==site]['latitude'][0])
+               lons.append(df[df.index==site]['longitude'][0])
+               sites.append(site)
 
-            # print lats,'\n',lons
-            filenum = (year - 2000) * 12 + month - 1
-            G = GridData(filenum)
-            pre = G.precip()
-            evi = G.evi()
-            ra = G.par()
-            ta = G.temperture()
-            kp = G.kp()
-            pft = G.pft()
+        # print lats,'\n',lons
+        filenum = (year - 2000) * 12 + month - 1
+        G = GridData(filenum)
+        pre = G.precip()
+        evi = G.evi()
+        ra = G.par()
+        ta = G.temperture()
+        kp = G.kp()
+        pft = G.pft()
+        fpar= G.fPAR(month-1,year-2000)
 
-            lat_ind,lon_ind = gridToPoint(lats,lons)
-            # print lat_ind,'\n',lon_ind
-            cnt=0
-            pre=np.ma.masked_where(pre<-10000,pre)
-            # plt.imshow(pre)
-            # plt.colorbar()
-            # plt.show()
+        lat_ind,lon_ind = gridToPoint(lats,lons)
+        # print lat_ind,'\n',lon_ind
+        cnt=0
+        # pre=np.ma.masked_where(pre<-10000,pre)
+        # plt.imshow(pre)
+        # plt.colorbar()
+        # plt.show()
 
-            for (r,c) in zip(lat_ind,lon_ind):
-                features[cnt,0] = pre[r,c]
-                features[cnt,1] = evi[r,c]
-                features[cnt,2] = ra[r,c]
-                features[cnt,3] = kp[r,c]
-                features[cnt,4] = pft[r,c]
-                features[cnt,5] = ta[r,c]
-                cnt+=1
+        for (r,c) in zip(lat_ind,lon_ind):
+            features[cnt,0] = pre[r,c]
+            features[cnt,1] = evi[r,c]
+            features[cnt,2] = ra[r,c]
+            features[cnt,3] = kp[r,c]
+            features[cnt,4] = pft[r,c]
+            features[cnt,5] = ta[r,c]
+            features[cnt,6] = fpar[r,c]
+            cnt+=1
 
-            def fillMissValue(vector):
-                temp=pd.DataFrame(vector)
-                temp=temp.fillna(temp.mean())
-                return temp.as_matrix()[:,0]
+        def fillMissValue(vector):
+            temp=pd.DataFrame(vector)
+            temp=temp.fillna(temp.mean())
+            return temp.as_matrix()[:,0]
 
-            features[:,1]=fillMissValue(features[:,1])
-            # print features[:,1]
-            # for i in range(0,6):
-                # print features[:,i]
-            clf=DecisionTreeClassifier()
-            clf=clf.fit(features,target)
+        features[:,1]=fillMissValue(features[:,1])
+        features[:,6]=fillMissValue(features[:,6])
+        # print features[:,1]
+        # for i in range(0,6):
+            # print features[:,i]
+        clf=DecisionTreeClassifier()
+        clf=clf.fit(features,target)
 
-        # os.unlink('iris.dot')
-        # dot_data=StringIO()
-        # tree.export_graphviz(clf,out_file=dot_data)
-        # graph=pydot.graph_from_dot_data(dot_data.getvalue())
-        # graph.write_pdf('training.pdf')
+    # os.unlink('iris.dot')
+    # dot_data=StringIO()
+    # tree.export_graphviz(clf,out_file=dot_data)
+    # graph=pydot.graph_from_dot_data(dot_data.getvalue())
+    # graph.write_pdf('training.pdf')
 
-            output=np.zeros([360,720],dtype=float)
-            output=np.ma.masked_where(pft==0,output)
+        output=np.zeros([360,720],dtype=float)
+        output=np.ma.masked_where(pft==0,output)
 
-            for r in range(0,360):
-                for c in range (0,720):
-                    if output[r,c]==0:
-                        features=np.zeros([1,6],dtype=float)
-                        if pre[r,c] is np.ma.masked:
-                            features[0,0]=0
-                        else:
-                            features[0,0] = pre[r, c]
+        for r in range(0,360):
+            for c in range (0,720):
+                if output[r,c]==0:
+                    features=np.zeros([1,7],dtype=float)
+                    if pre[r,c] is np.ma.masked:
+                        features[0,0]=0
+                    else:
+                        features[0,0] = pre[r, c]
 
-                        if evi[r,c] is np.ma.masked:
-                            features[0,1]=0
-                        else:
-                            features[0,1] = evi[r,c]
+                    if evi[r,c] is np.ma.masked:
+                        features[0,1]=0
+                    else:
+                        features[0,1] = evi[r,c]
 
-                        features[0,2] = ra[r, c]
-                        features[0,3] = kp[r, c]
-                        features[0,4] = pft[r, c]
-                        features[0,5] = ta[r, c]
-                        # print features
-                        output[r,c]=clf.predict(features)
+                    features[0,2] = ra[r, c]
+                    features[0,3] = kp[r, c]
+                    features[0,4] = pft[r, c]
+                    features[0,5] = ta[r, c]
+                    if fpar[r,c] is np.ma.masked:
+                        features[0,6]=0
+                    else:
+                        features[0,6]=fpar[r,c]
 
-            # output=np.ma.masked_where(output>9,output)
-            # output = np.ma.masked_where(output <0, output)
-            #calcalate total GPP
-            area_weight = io.loadmat(r'G:\Research\Gridded Data\NC\area_360x720.mat')
-            area = area_weight['area_global']
-            output = output * area / 1000000000000000
-            total_GPP = total_GPP + np.sum(np.sum(output)) * 30
-    print total_GPP/20.0
+
+                    output[r,c]=clf.predict(features)
+
+        # output=np.ma.masked_where(output>9,output)
+        # output = np.ma.masked_where(output <0, output)
+        #calcalate total GPP
+        area_weight = io.loadmat(r'G:\Research\Gridded Data\NC\area_360x720.mat')
+        area = area_weight['area_global']
+        output = output * area / 1000000000000000*30
+        output.dump('decisionTreeYear{0}.npy'.format(year))
+        del output
