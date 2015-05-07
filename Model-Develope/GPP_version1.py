b78b2c70f9c2c25a1277cf7f32856339a711e6cc
Sue Wei <suhua.wei00@gmail.com>
2015-05-07 13:30:29 -0400

    Update GPP_version1.py

--- Model-Develope/GPP_version1.py
+++ Model-Develope/GPP_version1.py
@@ -11,9 +11,9 @@ class GPP:
     '''GPP (Gross Primary Production) is calculated as GPP=alpha*fPAR*PAR; alpha is the specific light use efficiency at monthly step;PAR is partial
      active radiation ; fPAR is the fraction of partial active radiation '''
 
-    def fPAR(self,month):
+    def fPAR(self,month,year):
         '''fPar is an unitless value ranging from 0 to 1, with monthly step and 0.5 x 0.5 spatial resolution'''
-        hdf_files=glob.glob(r'G:\Research\Gridded Data\Vegetation Index\LAI and FPAR\2004to2005fPAR\*.HDF')
+        hdf_files=glob.glob(r'G:\Research\Gridded Data\Vegetation Index\LAI and FPAR\fPAR200{0}\*.HDF'.format(year)
         cnt=0
         for fn in hdf_files:
             if fn.endswith('HDF'):
@@ -28,20 +28,18 @@ class GPP:
         units: W/m^2; precision: 1'''
         f=netcdf.netcdf_file(r'G:\Research\Gridded Data\NC\NC_data\nswrs.sfc.mon.mean.nc','r')
         Var=f.variables['nswrs']
+        sta=np.linspace(636,756,11)
+        end=sta+12
         # Par is considered as 45% of the net shortwave radiation, divided by 11.58 to get MJ
-        data=Var[672:696,:,:]*0.45/11.58
+        data=Var[int(sta[year]):int(end[year]),:,:]*0.45/11.58
         #  Resize the data into 0.5x0.5 degree ([360,720])
         sr=-zoom(data[month-1,:,:],[360/94.0,720/192.0],mode='nearest')
-        # plt.imshow(sr)
-        # plt.colorbar()
-        # plt.show()
-        # print np.max(sr),np.min(sr),np.max(data[mon,:,:]),np.min(data[mon,:,:])
         return sr
 
     def Alpha(self,month):
         files=glob.glob('*.npy')
         for f in files:
-            if f.endswith('_{0}.npy'.format(month)):
+            if f.endswith('_{0}.npy'.format(month+1)):
                 alpha=np.load(f)
                 alpha=np.ma.masked_where(alpha==0,alpha)
         return alpha
@@ -50,33 +48,34 @@ class GPP:
 #--------------------------Main funtion starts here---------------------------------------------------------------------
 GPP=GPP()
 total_GPP=0
-for month in range(1,13):
-    fpar=GPP.fPAR(month)
-    PAR=GPP.PAR(month)
-    alpha=GPP.Alpha(month)
-    output=alpha*fpar*PAR
-    fig = plt.figure()
-    ax = fig.add_axes([0.05,0.05,0.9,0.9])
-    map=np.ma.fix_invalid(output,copy=True,fill_value=0)
-  
-    lats=np.linspace(90,-90,360)
-    lons=np.linspace(-180,180,720)
-    lons, lats = np.meshgrid(lons,lats)
-    m =basemap.Basemap(projection='robin',lon_0=0.0,resolution=None)
-    im1= m.pcolormesh(lons,lats,map,shading='flat',cmap=plt.cm.jet,latlon=True)
-    m.drawparallels(np.arange(-90.,99.,30.))
-    m.drawmeridians(np.arange(-180.,180.,60.))
-    mstr=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-    ax.set_title(''.join(['Global Gross Primary Production in ' , mstr[month-1]]))
-    cb = m.colorbar(im1,"bottom", size="5%", pad="2%")
-    plt.savefig(''.join(['Global Gross Primary Production in ' , mstr[month-1],'.jpg']))
-    # plt.show()
+for year in range(0,6):
+    for month in range(1,13):
+        fpar=GPP.fPAR(month,year)
+        PAR=GPP.PAR(month,year)
+        alpha=GPP.Alpha(month)
+        output=alpha*fpar*PAR
+        fig = plt.figure()
+        ax = fig.add_axes([0.05,0.05,0.9,0.9])
+        map=np.ma.fix_invalid(output,copy=True,fill_value=0)
+      
+        lats=np.linspace(90,-90,360)
+        lons=np.linspace(-180,180,720)
+        lons, lats = np.meshgrid(lons,lats)
+        m =basemap.Basemap(projection='robin',lon_0=0.0,resolution=None)
+        im1= m.pcolormesh(lons,lats,map,shading='flat',cmap=plt.cm.jet,latlon=True)
+        m.drawparallels(np.arange(-90.,99.,30.))
+        m.drawmeridians(np.arange(-180.,180.,60.))
+        mstr=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+        ax.set_title(''.join(['Global Gross Primary Production in ' , mstr[month-1]]))
+        cb = m.colorbar(im1,"bottom", size="5%", pad="2%")
+        plt.savefig(''.join(['Global Gross Primary Production in ' , mstr[month-1],'.jpg']))
+        # plt.show()
+        
+        area_weight=io.loadmat(r'G:\Research\Gridded Data\NC\area_360x720.mat')
+        area=area_weight['area_global']
+        output=output*area/1000000000000000
+        total_GPP=total_GPP+np.sum(np.sum(output))*30
+        print total_GPP
     
-    area_weight=io.loadmat(r'G:\Research\Gridded Data\NC\area_360x720.mat')
-    area=area_weight['area_global']
-    output=output*area/1000000000000000
-    total_GPP=total_GPP+np.sum(np.sum(output))*30
+    
 
-
-
-print total_GPP*0.85
